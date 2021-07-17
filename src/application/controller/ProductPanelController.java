492394b563e94ac898b7d9e81fbc2e320a9a1a5a
不想秃头的小姜 <1215683942@qq.com>
2021-07-17 22:41:26 +0800

    111

--- src/application/controller/ProductPanelController.java
+++ src/application/controller/ProductPanelController.java
@@ -25,15 +25,11 @@ import java.util.Map;
 public class ProductPanelController {
     private List<Product> products = null;
     private ProductDao dao = new ProductDaoImp();
-    /**
-     * cart type (just for u to know all the type)
-     */
+
     private final String[] cartType = {"鱼类", "狗类", "爬行类", "猫类", "鸟类"};
     @FXML
     private ComboBox<String> comboBox;
 
-    private Stage productPanelStage = new Stage();
-    private AnchorPane productPane;
     private MainApp mainApp;
     /**
      * creat a map used as save cart with type
@@ -43,49 +39,7 @@ public class ProductPanelController {
     /**
      * only when user successfully login this function will be called
      */
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
-        initProductPane();
-    }
 
-    public void initProductPane() {
-        //初始化ComboBox
-        comboBox = new ComboBox<>();
-
-        //add the cart type at comboBox but not work here,it actually worked in ProductPane.fxml
-        for (int i = 0; i < cartType.length; i++) {
-            comboBox.setValue(cartType[i]);
-        }
-        /*should do something to filed all the pane and massage*/
-
-        /*in the end show product panel*/
-        showProductPanel();
-    }
-
-    public void showProductPanel() {
-        FXMLLoader ProductPanelLoader = new FXMLLoader();
-        ProductPanelLoader.setLocation(MainApp.class.getResource("view/ProductPanel.fxml"));
-        try {
-            productPane = ProductPanelLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        productPanelStage.setScene(new Scene(productPane));
-        productPanelStage.setTitle("productPanel");
-        productPanelStage.show();
-    }
-
-    @FXML
-    private void reload() {
-        /*System.out.println("Action");
-        productPanelStage.hide();
-        productPanelStage.close();*/
-    }
-
-    @FXML
-    private void selectByType() {
-
-    }
 
 
 }
