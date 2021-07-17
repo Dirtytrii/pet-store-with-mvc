32420d7668a3cfa492e899e957c171d025e5ca91
不想秃头的小姜 <1215683942@qq.com>
2021-07-17 22:24:36 +0800

    111

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -1,8 +1,8 @@
 package application;
 
 import application.controller.ProductPanelController;
-import application.model.Account;
 import application.controller.SignInController;
+import application.model.Account;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,14 +15,56 @@ import java.io.IOException;
  * @author cloudJiang
  */
 public class MainApp extends Application {
-    public static Account account;
+    public Account account;
 
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    private Stage stage = new Stage();
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    /*
+    cannot new a controller to link mainApp and controller
     private SignInController signInController = new SignInController();
-    private ProductPanelController productPanelController = new ProductPanelController();
+    private ProductPanelController productPanelController = new ProductPanelController();*/
 
     @Override
     public void start(Stage primaryStage) {
-        signInController.setMainApp(this);
+        showLoginPane();
+    }
+
+    private void showLoginPane() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    MainApp.class.getResource("/view/signin.fxml"));
+            AnchorPane signInPane = loader.load();
+            SignInController controller = loader.getController();
+            controller.setMainApp(this);
+
+            Scene scene = new Scene(signInPane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        stage.setOnCloseRequest(event -> {
+            System.out.println("Closing Stage");
+        });
+    }
+
+    public void showProductPanel() {
+
     }
 
     public static void main(String[] args) {
--- src/application/controller/SignInController.java
+++ src/application/controller/SignInController.java
@@ -2,6 +2,8 @@ package application.controller;
 
 import application.MainApp;
 import dao.mysql.AccountDaoImp;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,42 +14,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
+/**
+ * @author DELL
+ */
 public class SignInController {
     @FXML
     private TextField user;
     @FXML
     private TextField password;
 
-    private AnchorPane signInBord;
-    private Stage signInStage = new Stage();
-
     private MainApp mainApp;
-    private ProductPanelController productPanelController = new ProductPanelController();
 
+    public MainApp getMainApp() {
+        return mainApp;
+    }
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
-        showSignIn();
-    }
-
-    //the interface show in MainApp.java
-    //return a AnchorPane to MainApp to show it
-    private void showSignIn() {
-        FXMLLoader SignLoader = new FXMLLoader();
-        SignLoader.setLocation(MainApp.class.getResource("view/Signin.fxml"));
-        try {
-            signInBord = SignLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //show sign in interface
-        signInStage.setScene(new Scene(signInBord));
-        signInStage.setTitle("UserLogin");
-        signInStage.show();
-
-        signInStage.setOnCloseRequest(event -> {
-            System.out.println("Closing Stage");
-        });
     }
 
     @FXML
@@ -55,24 +38,24 @@ public class SignInController {
         AccountDaoImp accountDaoImp = new AccountDaoImp();
         mainApp.account = accountDaoImp.findById(user.getText());
 
-        if (user.getText().equals("")) ;
-        else if (password.getText().equals("")) ;
-        else if (mainApp.account != null &&
+        if (user.getText().equals("")) {
+            ;
+        } else if (password.getText().equals("")) {
+            ;
+        } else if (mainApp.account != null &&
                 mainApp.account.getPassword().equals(password.getText())) {
             System.out.println("Success login!");
-            signInStage.hide();
-
-            //wait for sign in
-            //if success login,change scene
-            //change the sign panel to product panel
-            productPanelController.setMainApp(mainApp);
-
-        } else
+            mainApp.showProductPanel();
+        } else {
             System.out.println("Field to login!");
+        }
     }
 
+    /**
+     * Event Listener on Button[#btnCancel].onAction
+     */
     @FXML
-    private void handleClose() {
-        signInStage.close();
+    public void handleCancel(ActionEvent event) {
+        Platform.exit();
     }
 }
