3b7e8fe52732b560b07415d22083182e8caf003e
不想秃头的小姜 <1215683942@qq.com>
2021-07-26 14:22:59 +0800

    111

--- src/application/controller/MassagePanelController.java
+++ src/application/controller/MassagePanelController.java
@@ -1,4 +1,18 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+
+/**
+ * @author cloudJiang
+ */
 public class MassagePanelController {
+
+    @FXML
+    private void okButton(){
+
+    }
+    @FXML
+    private void bakeButton(){
+
+    }
 }
--- src/application/controller/ShoppingMassagePanelController.java
+++ src/application/controller/ShoppingMassagePanelController.java
@@ -1,4 +1,18 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * @author cloudJiang
+ */
 public class ShoppingMassagePanelController {
+    @FXML
+    Label userName;
+    @FXML
+    Label time;
+
+    public void showSMP(){
+
+    }
 }
